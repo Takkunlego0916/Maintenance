2b043fd2478c1652d6dbd99165fb49b2ab3e60e4
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-08-16 06:43:20 +0900

    Update ServerPingListener.java

--- src/main/java/io/github/takkunlego0916/maintenance/ServerPingListener.java
+++ src/main/java/io/github/takkunlego0916/maintenance/ServerPingListener.java
@@ -1,35 +1,52 @@
 package io.github.takkunlego0916.maintenance;
 
+import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.server.ServerListPingEvent;
 
 public class ServerPingListener implements Listener {
 
-    private final Maintenance plugin;
+    private static final LegacyComponentSerializer SERIALIZER = LegacyComponentSerializer.legacyAmpersand();
 
-    public ServerPingListener(Maintenance plugin) {
+    private final Maintenance plugin;
+    private final MaintenanceManager maintenanceManager;
+
+    public ServerPingListener(Maintenance plugin, MaintenanceManager maintenanceManager) {
         this.plugin = plugin;
+        this.maintenanceManager = maintenanceManager;
     }
 
     @EventHandler
-    public void onPing(ServerListPingEvent event) {
+    public void onPing(PaperServerListPingEvent event) {
 
-        boolean maintenance = plugin.getConfig().getBoolean("maintenance");
+        boolean maintenance = maintenanceManager.isEnabled();
+        String path = maintenance ? "motd.maintenance" : "motd.normal";
 
-        String line1 = plugin.getConfig().getString("motd.line1");
-        String line2;
+        String line1 = plugin.getConfig().getString(path + ".line1", "");
+        String line2 = plugin.getConfig().getString(path + ".line2", "");
 
-        if (maintenance) {
-            line2 = plugin.getConfig().getString("motd.maintenance-line2");
-        } else {
-            line2 = plugin.getConfig().getString("motd.line2");
+        line2 = line2.replace("%online%", String.valueOf(Bukkit.getOnlinePlayers().size()))
+                .replace("%max%", String.valueOf(Bukkit.getMaxPlayers()));
+
+        Component motd = SERIALIZER.deserialize(line1 + "\n" + line2);
+        event.motd(motd);
+
+        if (!maintenance) {
+            return;
         }
 
-        event.setMotd(color(line1) + "\n" + color(line2));
-    }
+        if (plugin.getConfig().getBoolean("motd.hide-player-count", true)) {
+            event.setHidePlayers(true);
+        }
 
-    private String color(String text) {
-        return text.replace("&", "§");
+        String versionText = plugin.getConfig().getString("motd.version-text", "");
+
+        if (!versionText.isBlank()) {
+            event.setProtocolVersion(-1);
+            event.setVersion(LegacyComponentSerializer.legacySection().serialize(SERIALIZER.deserialize(versionText)));
+        }
     }
 }
